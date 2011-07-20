b6327beb18dd17953ad6d70a9abc562e197947ae
Toru Yamaguchi <yamaguchi.toru@51645-MAC.local>
2011-07-21 01:53:39 +0900

    adhoc

---
+++ .bashrc
@@ -0,0 +1,21 @@
+# .bashrc
+
+# Source global definition
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# User specific aliases and functions
+alias less="/bin/less -r"
+alias ls="/bin/ls -GF"
+
+# User specific environment and startup programs
+LANG=ja_JP.UTF-8
+TZ=JST-9
+PAGER=less
+PS1="[\u@\h \t \w]$ "
+
+export LANG
+export TZ
+export PAGER
+export PS1
---
+++ link.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+DOT_FILES_DIR=`dirname $0`;
+
+# test -h $HOME/.emacs || ln -s $DOT_FILES_DIR/.emacs $HOME/.emacs
+### link directories under .emacs.d/*
+pushd $DOT_FILES_DIR > /dev/null;
+find . -regex ".*/\.[0-9a-zA-Z_]*$" -type f -depth 1 -print | xargs -I{} basename {} | xargs -I{} sh -c "test -e $HOME/{} || ln -s $DOT_FILES_DIR/{} $HOME/{}";
+find .emacs.d -type d -print | xargs -I{} sh -c "test -e $HOME/{} || ln -s $DOT_FILES_DIR/{} $HOME/{}";
+popd > /dev/null;
